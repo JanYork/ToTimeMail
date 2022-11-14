0498dc7bad04b14ed00c4e700bda416a73dfb3fb
JanYork <747945307@qq.com>
2022-11-14 23:35:12 +0800

    新增配置类

---
+++ docs/config/toTime.config.ts
@@ -0,0 +1,12 @@
+// 创建一个类保存配置信息
+class ToTimeConfig {
+        // 基础url前缀
+        // static baseURL = "http://localhost:8899";
+        static baseURL = "https://times.sady0.com";
+        // 基础文件上传api
+        static uploadURL = "http://localhost:8899/upload/file";
+        // 基础链接上传api
+        static linkURL = "http://localhost:8899/upload/fileUrl";
+}
+// 导出配置
+export default ToTimeConfig

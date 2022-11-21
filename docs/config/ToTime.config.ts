8846810e9e1fd4236951af0481ac73bf8759a4b3
JanYork <747945307@qq.com>
2022-11-21 16:26:02 +0800

    新增镜像站点配置

--- docs/config/ToTime.config.ts
+++ docs/config/ToTime.config.ts
@@ -2,11 +2,17 @@
 class ToTimeConfig {
         // 基础url前缀
         // static baseURL = "http://localhost:8899";
-        static baseURL = "https://times.sady0.com";
+        static baseURL = "https://api.totime.cn";
         // 基础文件上传api
-        static uploadURL = "http://localhost:8899/upload/file";
+        // static uploadURL = "http://localhost:8899/upload/file";
+        static uploadURL = "https://api.totime.cn/upload/file";
         // 基础链接上传api
-        static linkURL = "http://localhost:8899/upload/fileUrl";
+        // static linkURL = "http://localhost:8899/upload/fileUrl";
+        static linkURL = "https://api.totime.cn/upload/fileUrl";
+        //是否镜像站点
+        static isCopy = false;
+        //官方站点
+        static official = "https://mail.totime.cn";
 }
 // 导出配置
 export default ToTimeConfig

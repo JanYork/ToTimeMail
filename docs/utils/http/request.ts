3a8054c4f805763486cbe2994e5fc5becf806a2a
JanYork <747945307@qq.com>
2022-11-21 16:33:03 +0800

    axios拦截器实现防抖与节流

--- docs/utils/http/request.ts
+++ docs/utils/http/request.ts
@@ -4,6 +4,51 @@ import axios from 'axios'
 import qs from 'qs'
 import ToTimeConfig from '../../config/ToTime.config'
 
+// 请求对象
+const pendingMap = new Map<string, any>;
+
+const getRequestKey = (config = {}) => {
+    // @ts-ignore
+    const {url, method, params, data} = config
+    return [url, method, qs.stringify(params), qs.stringify(data)].join('&')
+}
+
+const addPendingRequest = (config) => {
+    const key = getRequestKey(config)
+    if (!pendingMap.has(key)) {
+        config.cancelToken = new axios.CancelToken(cancel => {
+            pendingMap.set(key, cancel)
+        })
+    }
+}
+
+const removePendingRequest = (config) => {
+    const key = getRequestKey(config)
+    if (pendingMap.has(key)) {
+        const cancelToken = pendingMap.get(key)
+        cancelToken(key)
+        pendingMap.delete(key)
+    }
+}
+
+/**
+ * 请求节流
+ * @param fn 请求函数
+ * @param delay 延迟时间
+ */
+const throttle = (fn, delay) => {
+    let timer = null;
+    return function () {
+        if (!timer) {
+            timer = setTimeout(() => {
+                // @ts-ignore
+                fn.apply(this, arguments);
+                timer = null;
+            }, delay);
+        }
+    }
+}
+
 //创建axios实例
 const service = axios.create({
     baseURL: ToTimeConfig.baseURL,
@@ -12,33 +57,37 @@ const service = axios.create({
 })
 
 // request拦截器
-service.interceptors.request.use(config => {
-    //如果是get请求，则使用qs.stringify()转换参数
-    if (config.method === 'get') {
-        config.params = config.params || {}
-        config.params = qs.stringify(config.params)
-    }
-    return config
-}, error => {
-    // Do something with request error
-    // @ts-ignore
-    return Promise.reject(error)
-    // Do something with request error
-    // return Promise.reject(error)
-})
+service.interceptors.request.use(
+    async config => {
+        //判断是否存在请求的key
+        if (pendingMap.has(getRequestKey(config))) {
+            //存在则取消请求
+            config.cancelToken = new axios.CancelToken(cancel => {
+                cancel('重复请求')
+            })
+        }
+        //不存在则添加到pendingMap中
+        addPendingRequest(config)
+
+        throttle(service, 10000)
+        return config
+    }, error => {
+        // @ts-ignore
+        return Promise.reject(error)
+    })
 
 //响应拦截器
 service.interceptors.response.use(
     response => {
-        // Do something with response data
+        //移除pendingMap中的请求
+        removePendingRequest(response.config)
         return response
     },
     error => {
-        // Do something with response error
+        // 移除重复请求
+        removePendingRequest(error.config)
         // @ts-ignore
         return Promise.reject(error)
-        // Do something with response error
-        // return Promise.reject(error)
     }
 )
 
